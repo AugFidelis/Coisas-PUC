1e33e6c56f5903716f6af190c08f32e5d09430bc
AugFidelis <augustofidelis2003@gmail.com>
2023-10-24 14:57:46 -0300

    Update 24/10

--- ASM/lab09_1.asm
+++ ASM/lab09_1.asm
@@ -15,6 +15,7 @@ PULALINHA MACRO
     MSG1 DB 'QUAL BASE SERA USADA? ("b" para binario, "h" para hexadecimal e "d" para decimal): $'
     INSIRAVALOR DB 13,10,'INSIRA O VALOR: $'
     INVALIDO DB 13,10,'VALOR INVALIDO!$'
+    MSGRES DB 13,10,'RESULTADO: $'
 
 .CODE
 MAIN PROC
@@ -23,13 +24,12 @@ MAIN PROC
 
     CALL LEITURA
 
-    ;CALL IMPRIMIR
-
-
+    MOV AH,4CH
+    INT 21H
 
 MAIN ENDP
 
-LEITURA PROC
+LEITURA PROC ;---------------------------------------------------------------------------------------------------
 
     LER:
     LEA DX,MSG1
@@ -55,21 +55,101 @@ LEITURA PROC
     JMP LER
 
     BIN:
-    CALL LERBIN
+    CALL ENTBIN
+    CALL SAIBIN
     RET
 
     HEX:
-    CALL LERHEX
+    ;CALL ENTHEX
+    ;CALL SAIHEX
     RET
 
     DECIM:
-    CALL LERDECIM
+    ;CALL ENTDEC
+    ;CALL SAIDEC
     RET
 
 LEITURA ENDP
 
-LERBIN PROC
+ENTBIN PROC ;----------------------------------------------------------------------------------------------------
 
+    LEA DX,INSIRAVALOR
+    MOV AH,09
+    INT 21H
+
+    XOR BX,BX
+    XOR CH,CH
+    
+    LER2:
+
+    MOV AH,01
+    INT 21H
+
+    CMP AL,0DH
+    JE SAIR
+    
+    CMP AL,30H
+    JB ERRO
+
+    CMP AL,31H
+    JA ERRO
+
+    AND AL,0FH
+    SHL BX,1
+    ADD BL,AL
+
+    INC CH
+    CMP CH,16
+    JE SAIR
+    
+    JMP LER2
+
+    ERRO:
+    LEA DX,INVALIDO
+    MOV AH,09
+    INT 21H
+    PULALINHA
+    JMP LER2
+    
+    SAIR:
+    RET
+
+ENTBIN ENDP ;----------------------------------------------------------------------------------------------------
+
+SAIBIN PROC ;----------------------------------------------------------------------------------------------------
+
+    LEA DX,MSGRES
+    MOV AH,09
+    INT 21H
+
+    MOV CX,16
+
+    IMP:
+    SHL BX,1
+    JC UM
+
+    MOV DL,30H
+    MOV AH,02
+    INT 21H
+    JMP SAIR2
+
+    UM:
+    MOV DL,31H
+    MOV AH,02
+    INT 21H
+    
+    SAIR2:
+    LOOP IMP
+    
+    RET
+
+SAIBIN ENDP ;----------------------------------------------------------------------------------------------------
+
+ENTHEX PROC ;----------------------------------------------------------------------------------------------------
+
+    XOR BX,BX
+
+    LER2:
     LEA DX,INSIRAVALOR
     MOV AH,09
     INT 21H
@@ -77,14 +157,36 @@ LERBIN PROC
     MOV AH,01
     INT 21H
 
+    CMP AL,0DH
+    JE SAIR
+
     CMP AL,30H
-    JB ERRO
+    JB ERRO2
 
-    CMP AL,31H
-    JA ERRO
+    CMP AL,3AH
+    JB CONT
 
+    CMP AL,41H
+    JB ERRO2
+
+    CMP AL,47H
+    JB CONT
+
+    ERRO2:
+    LEA DX,INVALIDO
+    MOV AH,09
+    INT 21H
+    PULALINHA
+    JMP LER2
+
+    CONT:
     
 
 
-LERBIN ENDP
+
+
+
+
+ENTHEX ENDP
+
 END MAIN
